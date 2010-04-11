8f1e60eaa0670cf4cea1795a7cdc87f61d5a6a91
Simon Thorneycroft <simon@simon-thorneycrofts-macbook-pro.local>
2010-04-11 16:07:35 +0100

    initial commit

---
+++ rakefile.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby -wKU
+
+require 'rake/testtask'
+
+Rake::TestTask.new('test') do |t|
+  t.pattern= 'test/**/tc_*.rb'
+  t.warning = true
+end
+
+desc "Build the bridge"
+task :build_bridge do
+  puts "Bridge built"
+end
+
+desc "Walk the bridge"
+task :walk_bridge => [:build_bridge] do
+  puts "Walked bridge"
+end
+
+task :default => [:walk_bridge]
+  
